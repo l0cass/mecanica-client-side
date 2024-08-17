bf7933ef7a7bbf4d875d44de344b85facac9f3cf
l0cass <piffer.lucas0@gmail.com>
2024-08-17 15:45:03 -0300

    chore: completed

---
+++ tailwind.config.js
@@ -0,0 +1,26 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+
+  theme: {
+    extend: {
+      colors: {
+        primary: "#1F3A93",
+        secundary: "#7F8C8D",
+
+        accent: "#E74C3C",
+        neutral: "#2C3E50",
+        background: "#ECF0F1",
+      },
+      backgroundImage: {
+        article: "url('article.jpg')",
+      },
+    },
+    container: {
+      padding: {
+        xl: "4rem",
+        DEFAULT: "1rem",
+      },
+    },
+  },
+};
